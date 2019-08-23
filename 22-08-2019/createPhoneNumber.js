97cfd91a4e3006c5b4e4757c026d0a2030cc80a6
kirankumar <kirankumarvijaya@gmail.com>
2019-08-23 10:43:23 +0530

    Fourth Task - CreatePhoneNumber

---
+++ 22-08-2019/createPhoneNumber.js
@@ -0,0 +1,11 @@
+// taken from internet stack overflow but understood the concept.
+//Will try to implement concept.
+
+function formatPhoneNumber(phoneNumberString) {
+    let cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+    let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if ( match ) {
+      return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+    }
+    return null
+  }
